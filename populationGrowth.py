6caefa968facc75a01f9c33e9434fb3ee7cd93cc
Kyle Haywood <kylehaywood@hotmail.com>
2024-01-16 16:40:51 -0500

    Not ideal but efficient as per achieve

--- populationGrowth.py
+++ populationGrowth.py
@@ -8,35 +8,50 @@ def dearAcheiveMyScriptDoesNotContainADingDangInfiniteLoopTYVM():
     newBirths = 0
     deathRate = 13
     deaths = 0
+    totaldeaths = 0
     immigrationRate = 45
     newImmigrants = 0
     totalGrowth = 0
     daysEllapsed = 0
 
-    ####### For the year 2024 (leap year) #######
-    # for i in range(1, secondsPerLeapYear+1):
+    # ####### For the year 2024 (leap year) #######
+    # # for i in range(1, secondsPerLeapYear+1):
 
-    #     if i % birthRate == 0: population=population+1; newBirths=newBirths+1
-    #     if i % deathRate == 0: population=population-1; deaths=deaths+1
-    #     if i % immigrationRate == 0: population=population+1; newImmigrants=newImmigrants+1
+    # #     if i % birthRate == 0: population=population+1; newBirths=newBirths+1
+    # #     if i % deathRate == 0: population=population-1; deaths=deaths+1
+    # #     if i % immigrationRate == 0: population=population+1; newImmigrants=newImmigrants+1
 
-    # daysEllapsed = daysEllapsed + (i//86400)
-    # totalGrowth = newBirths - deaths + newImmigrants
-    # print(f'The population at the start of 2024 is 312032486')
-    # print(f'By the end of year 2024, we will have seen {math.ceil(daysEllapsed)} days, Total population: {population}, new births: {newBirths}, deaths: {deaths}, new immigrants: {newImmigrants}, Total growth {totalGrowth}\n')
+    # # daysEllapsed = daysEllapsed + (i//86400)
+    # # totalGrowth = newBirths - deaths + newImmigrants
+    # # print(f'The population at the start of 2024 is 312032486')
+    # # print(f'By the end of year 2024, we will have seen {math.ceil(daysEllapsed)} days, Total population: {population}, new births: {newBirths}, deaths: {deaths}, new immigrants: {newImmigrants}, Total growth {totalGrowth}\n')
+
+    # for j in range(2024, 2029):
+    #     for i in range(1, secondsPerYear+1):
+
+    #         if i % birthRate == 0: population=population+1; newBirths=newBirths+1
+    #         if i % deathRate == 0: population=population-1; deaths=deaths+1
+    #         if i % immigrationRate == 0: population=population+1; newImmigrants=newImmigrants+1
+
+    #     daysEllapsed = daysEllapsed + (i//86400)
+    #     totalGrowth = newBirths - deaths + newImmigrants
+    #     print(f'By the end of year {j}, we will have seen {math.ceil(daysEllapsed)} days, The countries total population: {population}, New births: {newBirths}, Deaths: {deaths}, New immigrants: {newImmigrants}, Total growth {totalGrowth}\n')
+    # print(f'At an average population growth rate of {math.ceil(totalGrowth/daysEllapsed)} per day.')
+    # print(population)
+    # return population
 
     for j in range(2024, 2029):
-        for i in range(1, secondsPerYear+1):
+        births = secondsPerYear // birthRate
+        deaths = secondsPerYear // deathRate
+        immigrants = secondsPerYear // immigrationRate
 
-            if i % birthRate == 0: population=population+1; newBirths=newBirths+1
-            if i % deathRate == 0: population=population-1; deaths=deaths+1
-            if i % immigrationRate == 0: population=population+1; newImmigrants=newImmigrants+1
+        population += births - deaths + immigrants
+        newBirths += births
+        totaldeaths += deaths
+        newImmigrants += immigrants
 
-        daysEllapsed = daysEllapsed + (i//86400)
+        daysEllapsed += secondsPerYear // 86400
         totalGrowth = newBirths - deaths + newImmigrants
-        print(f'By the end of year {j}, we will have seen {math.ceil(daysEllapsed)} days, The countries total population: {population}, New births: {newBirths}, Deaths: {deaths}, New immigrants: {newImmigrants}, Total growth {totalGrowth}\n')
-    print(f'At an average population growth rate of {math.ceil(totalGrowth/daysEllapsed)} per day.')
-    print(population)
-    return population
+        print(f'By the end of year {j}, we will have seen {math.ceil(daysEllapsed)} days, The countries total population: {population}, New births: {newBirths}, Deaths: {totaldeaths}, New immigrants: {newImmigrants}, Total growth {totalGrowth}\n')
 
 dearAcheiveMyScriptDoesNotContainADingDangInfiniteLoopTYVM()
